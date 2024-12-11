c86ee8b2fc5e04c27e32e770bed28bed35087113
ForgeBrew96 <christianbmendoza96@gmail.com>
2024-12-11 15:02:24 -0500

    full crud complete, signup/signin back end set up

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, jsonify
+from auth_middleware import token_required
+from flask import Flask, request, jsonify, g
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+import jwt
+import bcrypt
 import psycopg2, psycopg2.extras
 import json
 
@@ -81,6 +84,21 @@ def index_cards():
   except:
     return "Application Error", 500
   
+@app.route('/cards/<card_id>', methods=['GET'])
+def show_card(card_id):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT * FROM cards WHERE card_id = %s", (card_id,))
+        card = cursor.fetchone()
+        if card is None:
+            connection.close()
+            return "Card Not Found", 404
+        connection.close()
+        return card, 200
+    except Exception as e:
+        return str(e), 500
+  
 @app.route('/cards', methods=['POST'])
 def create_cards():
     try:
@@ -106,4 +124,134 @@ def create_cards():
         print("Error:", e) 
         return str(e), 500
     
+@app.route('/cards/<card_id>', methods=['PUT'])
+def update_card(card_id):
+    try:
+      
+      effects_json = json.dumps(request.json['effects'])
+
+      connection = get_db_connection()
+      cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+      cursor.execute("UPDATE cards SET name = %s, point_value = %s, protocol_id = %s, effects = %s WHERE card_id = %s RETURNING *", (request.json['name'], request.json['point_value'], request.json['protocol_id'], effects_json, card_id))
+      updated_card = cursor.fetchone()
+      if updated_card is None:
+        return "Card Not Found", 404
+      connection.commit()
+      connection.close()
+      return updated_card, 202
+    except Exception as e:
+      return str(e), 500
+    
+@app.route('/cards/<card_id>', methods=['DELETE'])
+def delete_card(card_id):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("DELETE FROM cards WHERE card_id = %s", (card_id,))
+        if cursor.rowcount == 0:
+            return "Card not found", 404
+        connection.commit()
+        cursor.close()
+        return "Card deleted successfully", 204
+    except Exception as e:
+        return str(e), 500
+    
+
+#User Account CRUD=================================
+
+@app.route('/users')
+def index_users():
+  try:
+    connection = get_db_connection()
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cursor.execute("SELECT * FROM users;")
+    users = cursor.fetchall()
+    connection.close()
+    return users
+  except:
+    return "Application Error", 500
+  
+@app.route('/sign-token', methods=['POST'])
+def sign_token():
+    try:
+        user_data = request.get_json()
+        
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        
+        cursor.execute("SELECT * FROM users WHERE id = %s AND username = %s;", (user_data["id"], user_data["username"]))
+        user = cursor.fetchone()
+        
+        if user is None:
+            return jsonify({"error": "Invalid user."}), 401
+        
+        password_is_valid = bcrypt.checkpw(bytes(user_data["password"], 'utf-8'), bytes(user["password"], 'utf-8'))
+        
+        if not password_is_valid:
+            return jsonify({"error": "Invalid credentials."}), 401
+        
+        token = jwt.encode({"id": user["id"], "username": user["username"]}, os.getenv('JWT_SECRET'), algorithm="HS256")
+        
+        return jsonify({"token": f"Bearer {token}"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        connection.close()
+
+
+@app.route('/verify-token', methods=['POST'])
+def verify_token():
+    try:
+      token = request.headers.get('Authorization').split(' ')[1]
+      decoded_token = jwt.decode(token, os.getenv('JWT_SECRET'), algorithms="HS256")
+      return jsonify({"user": decoded_token })
+    except Exception as error:
+        return jsonify({"error": error.message})
+
+@app.route('/auth/signup', methods=['POST'])
+def signup():
+    try:
+        new_user_data = request.get_json()
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT * FROM users WHERE username = %s;", (new_user_data["username"],))
+        existing_user = cursor.fetchone()
+        if existing_user:
+            cursor.close()
+            return jsonify({"error": "Username already taken"}), 400
+        hashed_password = bcrypt.hashpw(bytes(new_user_data["password"], 'utf-8'), bcrypt.gensalt())
+        cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s) RETURNING username", (new_user_data["username"], hashed_password.decode('utf-8')))
+        created_user = cursor.fetchone()
+        connection.commit()
+        connection.close()
+        token = jwt.encode(created_user, os.getenv('JWT_SECRET'))
+        return jsonify({"token": token, "user": created_user}), 201
+    except Exception as error:
+        return jsonify({"error": str(error)}), 401
+
+@app.route('/auth/signin', methods=["POST"])
+def signin():
+    try:
+        sign_in_form_data = request.get_json()
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT * FROM users WHERE username = %s;", (sign_in_form_data["username"],))
+        existing_user = cursor.fetchone()
+        if existing_user is None:
+            return jsonify({"error": "Invalid credentials."}), 401
+        password_is_valid = bcrypt.checkpw(bytes(sign_in_form_data["password"], 'utf-8'), bytes(existing_user["password"], 'utf-8'))
+        if not password_is_valid:
+            return jsonify({"error": "Invalid credentials."}), 401
+        token = jwt.encode({"username": existing_user["username"], "id": existing_user["id"]}, os.getenv('JWT_SECRET'))
+        return jsonify({"token": token}), 201
+    except Exception as error:
+        return jsonify({"error": "Invalid credentials."}), 401
+    finally:
+        connection.close()
+
+@app.route('/vip-lounge')
+@token_required
+def vip_lounge():
+    return f"Welcome to the party, {g.user['username']}"
+
 app.run()
